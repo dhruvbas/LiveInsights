d8a2330ca8eeafe022ed22a111b304c70ac05a1b
dhruvbas <dhruv.bas@gmail.com>
2020-06-25 02:05:50 -0400

    Add files via upload

---
+++ include/Footer.php
@@ -0,0 +1,73 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Our Newsletter</h4>
+            <p>Keep updated for new releases and freebies. Enter your e-mail and subscribe to our newsletter.</p>
+          </div>
+          <div  class="col-lg-6">
+			<div id = "subscribe">
+				<form action="forms/subscribe.php" method="post" role = "form" class = "php-email-form">
+				  <input type="email" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" ><input type="submit" value="Subscribe">
+				</form>
+			</div>
+		   <div id="subscribsucessemessage"><p>Thank you for subscribing our newsletter.</p></div>
+          <div id="subscribeerrormessage"><p></p></div>	
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Browse Pages</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="products.php">Products</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="company.php">Company</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="https://medium.com/@LiveInsights" target="_blank">Blog</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact Us</a></li>
+              
+            </ul>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              Live Insights Inc. <br>
+              33 Lombard Street<br>
+              Toronto M5C 3H8 Canada <br><br>
+              <strong>Email:</strong>  contact@liveinsights.ai<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>About Live Insights</h3>
+            <p>We provide data analytics solutions to modern, consumer-focussed companies. Our team leverage machine learning and artificial intelligence to improve demand forecasting, scheduling, inventory management to name a few.</p>
+            <div class="social-links mt-3">
+              <a href="https://twitter.com/insights_live" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="https://www.facebook.com/LiveInsights-956248711386271/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.linkedin.com/company/liveinsights-ai/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Live Insights Inc.</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+  </footer><!-- End Footer -->
---
+++ include/Header.php
@@ -0,0 +1,31 @@
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+      
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+			<li class= "<?php echo $HomePage ?>"><a href="index.php">Home</a></li>
+			<li class="dropdown <?php echo $ProductsPage ?>">
+			  <a href="products.php">Products</a>
+			</li>
+			<li class="dropdown <?php echo $CompanyPage ?>">
+			  <a href="company.php">Company</a>
+			</li>
+			<li class="dropdown <?php echo $Blog ?>">
+			  <a href="https://medium.com/@LiveInsights" target="_blank">Blog</a>
+			</li>
+			<li class="dropdown <?php echo $ContactPage ?>">
+			  <a href="contact.php">Contact</a>
+			</li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
